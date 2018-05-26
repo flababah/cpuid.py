c12b18449ea093ada64a66c078f7661012f2656b
xuyu <vatiminxuyu@gmail.com>
2018-05-26 11:04:37 +0800

    Add examples calling cpuid with EAX and ECX

--- example.py
+++ example.py
@@ -16,8 +16,14 @@ def cpu_name(cpu):
     return "".join((struct.pack("IIII", *cpu(0x80000000 + i)).decode("utf-8")
             for i in range(2, 5))).strip()
 
-def is_set(cpu, id, reg_idx, bit):
-    regs = cpu(id)
+"""
+@param {leaf} %eax
+@param {sublead} %ecx, 0 in most cases
+@param {reg_idx} idx of [%eax, %ebx, %ecx, %edx], 0-based
+@param {bit} bit of reg selected by {reg_idx}, 0-based
+"""
+def is_set(cpu, leaf, subleaf, reg_idx, bit):
+    regs = cpu(leaf, subleaf)
 
     if (1 << bit) & regs[reg_idx]:
         return "Yes"
@@ -31,15 +37,19 @@ if __name__ == "__main__":
     print("CPU name  : %s" % cpu_name(cpu))
     print()
     print("Vector instructions supported:")
-    print("SSE       : %s" % is_set(cpu, 1, 3, 25))
-    print("SSE2      : %s" % is_set(cpu, 1, 3, 26))
-    print("SSE3      : %s" % is_set(cpu, 1, 2, 0))
-    print("SSSE3     : %s" % is_set(cpu, 1, 2, 9))
-    print("SSE4.1    : %s" % is_set(cpu, 1, 2, 19))
-    print("SSE4.2    : %s" % is_set(cpu, 1, 2, 20))
-    print("SSE4a     : %s" % is_set(cpu, 0x80000001, 2, 6))
-    print("AVX       : %s" % is_set(cpu, 1, 2, 28))
-    print("AVX2      : %s" % is_set(cpu, 7, 1, 5))
-    print("BMI1      : %s" % is_set(cpu, 7, 1, 3))
-    print("BMI2      : %s" % is_set(cpu, 7, 1, 8))
-
+    print("SSE       : %s" % is_set(cpu, 1, 0, 3, 25))
+    print("SSE2      : %s" % is_set(cpu, 1, 0, 3, 26))
+    print("SSE3      : %s" % is_set(cpu, 1, 0, 2, 0))
+    print("SSSE3     : %s" % is_set(cpu, 1, 0, 2, 9))
+    print("SSE4.1    : %s" % is_set(cpu, 1, 0, 2, 19))
+    print("SSE4.2    : %s" % is_set(cpu, 1, 0, 2, 20))
+    print("SSE4a     : %s" % is_set(cpu, 0x80000001, 0, 2, 6))
+    print("AVX       : %s" % is_set(cpu, 1, 0, 2, 28))
+    print("AVX2      : %s" % is_set(cpu, 7, 0, 1, 5))
+    print("BMI1      : %s" % is_set(cpu, 7, 0, 1, 3))
+    print("BMI2      : %s" % is_set(cpu, 7, 0, 1, 8))
+    # Intel RDT CMT/MBM
+    print("L3 Monitoring : %s" % is_set(cpu, 0xf, 0, 3, 1))
+    print("L3 Occupancy  : %s" % is_set(cpu, 0xf, 1, 3, 0))
+    print("L3 Total BW   : %s" % is_set(cpu, 0xf, 1, 3, 1))
+    print("L3 Local BW   : %s" % is_set(cpu, 0xf, 1, 3, 2))
